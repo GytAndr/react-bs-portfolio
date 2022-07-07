5192558f31fe8e017faa349b5c41bbe7bdd9551b
GytAndr <andriejunas@gmail.com>
2022-07-07 18:01:20 +0300

    initial commit + Navigation

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,37 @@
+//React Bootstrap imports:
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Image from 'react-bootstrap/Image';
+import Nav from 'react-bootstrap/Nav';
+//Images import
+import profilePic from '../images/50x50.png';
+export default function Navigation() {
+	return (
+		<Navbar bg="light" expand="lg" variant="light">
+			<Container>
+				<Navbar.Brand href="#home">
+					<Image
+						roundedCircle
+						className="align-middle me-2"
+						src={profilePic}
+						alt="Profile"
+					/>
+					Portfolio
+				</Navbar.Brand>
+				<Navbar.Toggle>
+					<span className="navbar-toggler-icon"></span>
+				</Navbar.Toggle>
+				<Navbar.Collapse>
+					<Nav className="ms-auto">
+						<Nav.Item className="active">
+							<Nav.Link href="#home">Home</Nav.Link>
+						</Nav.Item>
+						<Nav.Item>
+							<Nav.Link href="#portfolio">Portfolio</Nav.Link>
+						</Nav.Item>
+					</Nav>
+				</Navbar.Collapse>
+			</Container>
+		</Navbar>
+	);
+}
