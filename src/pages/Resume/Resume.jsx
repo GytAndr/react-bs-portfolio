5e2baa7a9a7dc685d0fab99f2898526d4886af61
GytAndr <andriejunas@gmail.com>
2022-07-18 12:47:00 +0300

    added Education

--- src/pages/Resume/Resume.jsx
+++ src/pages/Resume/Resume.jsx
@@ -1,9 +1,9 @@
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/esm/Row';
 import Col from 'react-bootstrap/esm/Col';
-
 import About from './components/About';
 import WorkExp from './components/WorkExp';
+import Education from './components/Education';
 export default function Resume() {
 	return (
 		<Container as="main" className="mt-4">
@@ -11,6 +11,7 @@ export default function Resume() {
 				<Col lg="8" className="col-12">
 					<About />
 					<WorkExp />
+					<Education />
 				</Col>
 			</Row>
 		</Container>
--- src/pages/Resume/components/Education.jsx
+++ src/pages/Resume/components/Education.jsx
@@ -1,5 +1,62 @@
-import React from 'react';
+import Card from 'react-bootstrap/esm/Card';
 
 export default function Education() {
-	return <div>Education</div>;
+	return (
+		<Card className="mb-4">
+			<Card.Header as="h2" className="text-center">
+				Education
+			</Card.Header>
+			<Card.Body>
+				<div className="d-flex align-items-start mb-2">
+					<i className="bi bi-book text-primary display-6 me-3"></i>
+					<div className="flex-grow-1">
+						<h4 className=" mb-1">Compliance Team Lead</h4>
+						<div className="d-sm-flex justify-content-sm-between align-items-sm-baseline">
+							<h5 className="mb-0">
+								<a
+									href="https://guidehouse.com/"
+									target="_blank"
+									className="text-decoration-none"
+								>
+									Guidehouse
+								</a>
+							</h5>
+							<small className="text-uppercase fw-bold text-muted">
+								<time>May 2021</time> &ndash; <time>January 2022</time>
+							</small>
+						</div>
+					</div>
+				</div>
+				<Card.Text>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+					eiusmod tempor incididunt ut labore et dolore magna aliqua.
+				</Card.Text>
+				<hr />
+				<div className="d-flex align-items-start mb-2">
+					<i className="bi bi-book text-primary display-6 me-3"></i>
+					<div className="flex-grow-1">
+						<h4 className=" mb-1">Compliance Team Lead</h4>
+						<div className="d-sm-flex justify-content-sm-between align-items-sm-baseline">
+							<h5 className="mb-0">
+								<a
+									href="https://guidehouse.com/"
+									target="_blank"
+									className="text-decoration-none"
+								>
+									Guidehouse
+								</a>
+							</h5>
+							<small className="text-uppercase fw-bold text-muted">
+								<time>May 2021</time> &ndash; <time>January 2022</time>
+							</small>
+						</div>
+					</div>
+				</div>
+				<Card.Text>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+					eiusmod tempor incididunt ut labore et dolore magna aliqua.
+				</Card.Text>
+			</Card.Body>
+		</Card>
+	);
 }
