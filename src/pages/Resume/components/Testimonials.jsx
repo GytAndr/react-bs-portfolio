e7d10856e0af089d7410621ef5e042e3fd0c9c4c
GytAndr <andriejunas@gmail.com>
2022-07-18 16:24:59 +0300

    recommendations filled

--- src/pages/Resume/components/Testimonials.jsx
+++ src/pages/Resume/components/Testimonials.jsx
@@ -14,12 +14,17 @@ export default function Testimonials() {
 						<Figure className="text-center mb-4 ">
 							<blockquote className="blockquote">
 								<p>
-									1111Lorem ipsum dolor sit amet consectetur adipisicing elit.
-									Excepturi, hic?
+									Gytis has successfully built well performing and engaged team.
+									Besides his robust skillset to successfully drive team's daily
+									operations he has also grown his leadership competencies.
+									Extensive data analysis and Power BI knowledge has well served
+									the whole workstream in data reporting and analytics. Always
+									professional, result oriented, fact based decision maker- it
+									was a pleasure to work with you!
 								</p>
 							</blockquote>
 							<figcaption className="blockquote-footer">
-								Name Surname
+								Agne Asauske, former Manager at Guidehouse 2021
 							</figcaption>
 						</Figure>
 					</Carousel.Item>
@@ -27,12 +32,16 @@ export default function Testimonials() {
 						<Figure className="text-center mb-4">
 							<blockquote className="blockquote">
 								<p>
-									2222Lorem ipsum dolor sit amet consectetur adipisicing elit.
-									Excepturi, hic?
+									Gytis is wonderful to work with, and has expertise in
+									compliance, great attention to detail. I was impressed by
+									Gytis ability to handle any situation calmly and patiently,
+									even in the toughest situations. This natural skill of his has
+									helped my team grow and build trustworthy environment. I’m
+									happy I got the chance to work with him.
 								</p>
 							</blockquote>
 							<figcaption className="blockquote-footer">
-								Name Surname
+								Deimante Miksiene, former Supervisor at Western Union 2021
 							</figcaption>
 						</Figure>
 					</Carousel.Item>
@@ -40,12 +49,20 @@ export default function Testimonials() {
 						<Figure className="text-center mb-4">
 							<blockquote className="blockquote">
 								<p>
-									333Lorem ipsum dolor sit amet consectetur adipisicing elit.
-									Excepturi, hic?
+									Gytis is an exceptional employee and colleague who has always
+									stood out in delivering expected results, going above and
+									beyond in assigned project or task with consistency, showing
+									his fast learning abilities and always ready and available to
+									take new challenges and rend help when needed. He has greatly
+									mastered the skill of complex suspicious activity
+									investigations and SARs preparation. He is a true inspiration
+									to the whole team as he has also developed his leadership
+									capabilities with multiple initiatives within the team. I look
+									forward to working with you in the future.
 								</p>
 							</blockquote>
 							<figcaption className="blockquote-footer">
-								Name Surname
+								Musa Malone Lawal, former Supervisor at Wester Union 2020
 							</figcaption>
 						</Figure>
 					</Carousel.Item>
